64f1405c31b5da2581b382a24bfdbec20dc18d15
RHNRaul <derpymore86@gmail.com>
2025-04-22 17:50:12 -0600

    Modificaciones

--- src/test/java/PruebasPrincipal.java
+++ src/test/java/PruebasPrincipal.java
@@ -1,4 +1,20 @@
+import javax.swing.JFrame;
+
+import rhn.convertir.Convertidor;
 
 public class PruebasPrincipal {
 
+	
+	
+	public static void main (String[] args) {
+		Convertidor co = new Convertidor();
+		co.escogerPorLotes(new JFrame());
+		//co.escogerCarpeta(new JFrame());
+		//co.escogerImagen(new JFrame());
+		co.escogerDestino(new JFrame());
+		co.convertirImagen("jpg");
+	}
+	
+	
+	
 }
